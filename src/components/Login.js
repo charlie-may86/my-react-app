ba0f245e4b63b6ef688ae033660619519e2d42d4
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-07-09 10:07:11 -0500

    form built

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react' 
+// import axios from 'axios'
+
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <input
+                    type='text'
+                    name='username'
+                    value='type here'
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Login

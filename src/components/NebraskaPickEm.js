e7f2ca88226e661beeba1a7d62eb3662879f6d59
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-08-09 14:10:41 -0500

    returning date how I want it

--- src/components/NebraskaPickEm.js
+++ src/components/NebraskaPickEm.js
@@ -1,30 +1,36 @@
 import React, { useContext } from "react";
 
-import { NebContext} from "./CollegeFootball";
+import { NebContext } from "./CollegeFootball";
 
 const NebPickEm = () => {
-  const {myData, isLoading} = useContext(NebContext);
+  const { myData, isLoading } = useContext(NebContext);
   console.log(myData);
   console.log(isLoading);
 
-
   const skedyFactory = () => {
-    myData.forEach((x) => {
-      return <div>{x.DateTime}</div>;
-    });
+    return (
+      <>
+        {myData.map((x) => {
+          if (x.DateTime) {
+            return (
+              <div>{new Date(x.DateTime).toDateString().slice(0, 10)}</div>
+            );
+          }
+          return <div>TBD</div>;
+        })}
+      </>
+    );
   };
 
-if(!isLoading) {
-    return <div>loading</div>
-} else {
-    return(
-        <div>
-            <div>{new Date(myData[0].DateTime).toDateString()}</div>
-        </div>
-    )
-}
-
-  
+  if (!isLoading) {
+    return <div>loading</div>;
+  } else {
+    return (
+      <div>
+        <div>{skedyFactory()}</div>
+      </div>
+    );
+  }
 };
 
 export default NebPickEm;

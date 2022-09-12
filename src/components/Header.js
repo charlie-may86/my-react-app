bb37c36325e0b17f7cc9d6e6eb360d5be4fef518
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-09-12 13:56:40 -0500

    styling header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Charlie May</h1>
-      <div className="header_nav">
-        <nav>
-          <ul>
-            <li>
-              <a href="http://localhost:3000/Resume">CV</a>
-            </li>
-            <li>
-              <a href="http://localhost:3000/Resume">Chess Game</a>
-            </li>
-            <li>
-              <a href="http://localhost:3000/cfb_pick_em">CFB Pickem</a>
-            </li>
-          </ul>
-        </nav>
-        <a href="http://localhost:3000/Resume" class="aboutMe">
-          <span />
-          About Me
-        </a>
+      <div className="headerName">
+        <h1>Charlie May</h1>
+      </div>
+      <div className="headerNav">
+        <a href="http://localhost:3000/Resume">CV</a>
+        <a href="http://localhost:3000/Resume">Chess Game</a>
+        <a href="http://localhost:3000/cfb_pick_em">CFB Pickem</a>
       </div>
     </header>
   );
